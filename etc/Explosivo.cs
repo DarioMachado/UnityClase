085d7f660193bfd0b64212221f09bd241d32f2f7
DarioMachado <115183065+DarioMachado@users.noreply.github.com>
2023-11-09 17:03:13 +0100

    area de explosion añadida (provisional)

--- etc/Explosivo.cs
+++ etc/Explosivo.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Explosivo : MonoBehaviour
 {
-    private const float RANGO_EXPLOSION = 30F;//Esto da por hecho que el rango de todos los explosivos serán iguales, puede ser cambiado luego quitando el const y creando getters y setters.
+    private const float RANGO_EXPLOSION = 5F;//Esto da por hecho que el rango de todos los explosivos serán iguales, puede ser cambiado luego quitando el const y creando getters y setters.
     private const int DMG_EXPLOSION = 60; //Lo mismo que el de arriba, pero con daño.
+    public GameObject bolaExplosion;
 
     public void Explotar() {
+
+        GameObject damageArea = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        damageArea.transform.position = transform.position;
+        damageArea.transform.localScale = new Vector3(RANGO_EXPLOSION * 2, RANGO_EXPLOSION * 2, RANGO_EXPLOSION * 2);
+        Renderer damageAreaRenderer = damageArea.GetComponent<Renderer>();
+        damageAreaRenderer.GetComponent<Renderer>().material.color = new Color(1f, 0, 0, 0.2f);
+        Destroy(damageArea, 1.2f);
         Collider[] colliders = Physics.OverlapSphere(transform.position, RANGO_EXPLOSION); //Detecta todos los objetos con colliders en el rango de RANGO_EXPLOSIVO
         foreach (Collider collider in colliders) 
         {
